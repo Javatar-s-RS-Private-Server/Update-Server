2b05cd72296ad9f3b57bcf37f764a834aed22d3e
David <davidschlachter96@gmail.com>
2021-04-11 21:09:34 -0700

    Removed unused dependencies.

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,14 +7,12 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
-    maven("https://repo.runelite.net")
 }
 
 dependencies {
     implementation(kotlin("stdlib"))
     implementation("com.displee:rs-cache-library:6.8")
     implementation("com.displee:disio:2.2")
-    implementation("net.runelite:cache:1.7.4")
     implementation("io.netty:netty-all:4.1.63.Final")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.3")
 }
